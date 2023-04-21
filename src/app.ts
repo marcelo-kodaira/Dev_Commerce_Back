bd6fdb39dc70411775b96bb830e87c3b80e9b86a
Marcelo Kodaira <marcelo.aallmeida@gmail.com>
2023-04-21 18:34:30 -0300

    Feat-Tests: Added Email Verification

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,16 @@ import 'express-async-errors';
 import express from 'express';
 import { appRoutes } from './routes';
 import handleErrorMiddleware from './middlewares/Error.middleware';
+import cors from 'cors';
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 
 appRoutes(app);
+
 app.use(handleErrorMiddleware);
 
 export default app;
--- src/services/user/createUser.service.ts
+++ src/services/user/createUser.service.ts
@@ -1,10 +1,21 @@
 import bcrypt from "bcrypt";
 import { ICreateUserRequest, IUserResponse } from "../../interfaces/user";
 import { prisma } from "../../prisma";
+import ErrorHttp from "../../error/Error";
 
 export const createUserService = async (data: ICreateUserRequest):Promise<IUserResponse> => {
   const passwordHash = bcrypt.hashSync(data.password, 10);
   const { password, ...rest } = data;
+
+  const emailAlreadyExist = await prisma.users.findUnique({
+    where:{
+      email: data.email
+    }
+  })
+
+  if(emailAlreadyExist){
+    throw new ErrorHttp("Email already registered",403)
+  }
 
   const user = await prisma.users.create({
     data: {
